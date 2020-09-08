c927a76c25734c425f7c0984d0f11568b557c9ee
Gaurav Ochani <GOchani@slb.com>
2020-09-08 21:12:59 +0530

    Done with second class

---
+++ test.js
@@ -0,0 +1,97 @@
+
+
+
+// uncomment to execute the code-block.
+
+// var createTrianglePyramid = (rows) => {
+//     for (let i = 0; i < rows; i++) {
+//         var output = '';
+//         for (let j = 0; j < rows - i; j++) output += ' ';
+//         for (let k = 0; k <= i; k++) output += '* ';
+//         console.log(output);
+//     }
+// }
+
+// createTrianglePyramid(5)
+
+//Anonymous function
+// (() => {
+//     console.log("wetesterr")
+// })()
+
+//ES6
+// var rajesh = () => {
+//     console.log("wetesterr")
+// }
+// rajesh()
+
+//ES5
+// var rajesh = function () {
+//     console.log("wetesterr")
+// }
+// rajesh()
+
+// let i = 0;
+// do {
+//     console.log(i)
+//     i++
+// } while (i < 3);
+
+
+// let i = 5
+// while (i <= 10) {
+//     break;
+
+// }
+
+// for (i = 5; i <= 10; i++) {
+
+// }
+// do {
+
+// } while (condition);
+// let a = 5
+// a = 6
+// console.log(a)
+
+// //ES5
+// function add() {
+//     var a = 10
+//     var b = 20
+//     var c = a + b
+//     console.log("sum = " + c)
+// }
+
+// // add()
+
+// //ES6
+
+// var addnew = () => {
+//     var a = 10
+//     var b = 20
+//     var c = a + b
+//     console.log("sum = " + c)
+// }
+
+// addnew()
+
+// var fact = () => {
+//     var num = 10
+//     var fact1 = 1
+//     for (var i = 1; i <= num; i++) { fact1 = fact1 * i }
+//     console.log("factorial is " + fact1)
+// }
+
+// fact()
+
+// var swap = () => {
+//     var num1 = 5
+//     var num2 = 6
+//     var temp
+//     temp = num1
+//     num1 = num2
+//     num2 = temp
+//     console.log("After Swap " + num1 + " " + num2)
+// }
+
+// swap()
