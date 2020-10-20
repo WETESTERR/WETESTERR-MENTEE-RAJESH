f17f2199e3efcd0390034d62a96ca41c53838f6c
Gaurav Ochani <gauravochani18@gmail.com>
2020-10-20 21:49:11 +0530

    added class 7 notes

---
+++ class78/test.js
@@ -0,0 +1,65 @@
+// uncomment to execute the code-block.
+
+
+//this
+
+// function bike() {
+//     var name = "Ninja"
+//     this.maker = "Kawasaki"
+//     console.log(this.name + " " + maker)
+// }
+
+// var name = "Pulsar"
+// var maker = "Bajaj"
+
+// var obj = new bike()
+// console.log(obj.maker)
+
+
+
+
+// function abc() {
+//         this.def = function () {
+//             console.log("hello");
+//         };
+//     }
+
+// var a = new abc()
+// a.def()
+
+
+
+
+
+
+// var obj1 = {
+//     name:"rajesh",
+//     age: function(){
+//         console.log(this.name)
+//     }
+// }
+
+// var obj2 = {
+//     name:"gaurav",
+//     age: obj1.age
+// }
+
+// var name = "Kiran"
+// var age = obj1.age
+
+// age()
+// obj1.age()
+// obj2.age()
+
+
+
+
+//Global variable
+// var a = 10
+
+// function abc() {
+//     a = 20
+//     console.log(a)
+// }
+// abc()
+
